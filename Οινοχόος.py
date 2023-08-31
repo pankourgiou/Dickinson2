0f72e5a90b209fda92603526faa4b2b341cf96b5
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-08-31 21:26:33 +0300

    Create Οινοχόος.py

---
+++ Οινοχόος.py
@@ -0,0 +1,2 @@
+#The narration is a palindrome! This means you can read it forwards and then you can reverse it. The whole script as translated in Greek, talks about a cupbearer meaning Οινοχόος in Greek language. Strange thing here is that Greek word Οινοχόος is similar in sound with the chinese greeting
+#你好. This leads me to the idea that Faistos disk is a greeting, like a coded hello world program. 
